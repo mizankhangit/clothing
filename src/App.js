b4d84df6e6d7fbeecfc800cabb57cc51b25bc672
mizankhangit <mizanurmgt@gmail.com>
2020-07-02 06:55:34 +0600

    collection preview and collection item components

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import Homepage from "./pages/homepage/Homepage";
 import "./App.css";
 import { Route, Switch } from "react-router-dom";
+import ShopPage from "./pages/shop/Shop";
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={Homepage} />
+        <Route exact path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
